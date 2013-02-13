ca105ef24227eee0f7c72ab295fcd7b20099de1f
vogias <vogias@admin.grnet.gr>
2013-02-13 11:23:47 +0200

    c

--- NE_Repository/src/org/ariadne_eu/utils/lucene/document/ne/NEHandler.java
+++ NE_Repository/src/org/ariadne_eu/utils/lucene/document/ne/NEHandler.java
@@ -27,9 +27,8 @@ public class NEHandler extends DocumentHandler {
 
 	private Document doc;
 
-	headerType headerType;
-
 	String content;
+	String tmpValue;
 
 	@Override
 	public Document getDocument(InputStream is) throws DocumentHandlerException {
@@ -54,7 +53,6 @@ public class NEHandler extends DocumentHandler {
 
 	public void startDocument() {
 		doc = new Document();
-		headerType = new headerType();
 		// content = new String();
 
 	}
@@ -67,193 +65,282 @@ public class NEHandler extends DocumentHandler {
 			Attributes atts) throws SAXException {
 
 		// Check for element existence in the header section
-		chechHeaderSection(qName);
+		checkHeaderSection(qName);
+		checkMetadataSection(qName, atts);
 
 	}
 
-	public void chechHeaderSection(String qName) {
+	public void checkMetadataSection(String qName, Attributes atts) {
+
+		if (qName.equalsIgnoreCase("ne:provider")
+				|| qName.equalsIgnoreCase("provider")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:dataProvider")
+				|| qName.equalsIgnoreCase("dataProvider")) {
+			
+
+		}
+		if (qName.equalsIgnoreCase("ne:objectUri")
+				|| qName.equalsIgnoreCase("objectUri")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:contextUri")
+				|| qName.equalsIgnoreCase("contextUri")) {
+			
+		}
+
+		if (qName.equalsIgnoreCase("ne:contentType")
+				|| qName.equalsIgnoreCase("contentType")) {
+			
+
+		}
+		if (qName.equalsIgnoreCase("ne:licenseUri")
+				|| qName.equalsIgnoreCase("licenseUri")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:thumbnailUri")
+				|| qName.equalsIgnoreCase("thumbnailUri")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:scientificName")
+				|| qName.equalsIgnoreCase("scientificName")) {
+			
+			atts.getValue("flag");
+			atts.getValue("annotation");
+			atts.getValue("vocab");
+			atts.getValue("uri");
+			atts.getValue("xml:lang");
+
+		}
+		if (qName.equalsIgnoreCase("ne:classification")
+				|| qName.equalsIgnoreCase("classification")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:commonName")
+				|| qName.equalsIgnoreCase("commonName")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:title")
+				|| qName.equalsIgnoreCase("title")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:creator")
+				|| qName.equalsIgnoreCase("creator")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:subject")
+				|| qName.equalsIgnoreCase("subject")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:description")
+				|| qName.equalsIgnoreCase("description")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:contributor")
+				|| qName.equalsIgnoreCase("contributor")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:date") || qName.equalsIgnoreCase("date")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:type") || qName.equalsIgnoreCase("type")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:format")
+				|| qName.equalsIgnoreCase("format")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:identifier")
+				|| qName.equalsIgnoreCase("identifier")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:source")
+				|| qName.equalsIgnoreCase("source")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:language")
+				|| qName.equalsIgnoreCase("language")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:relation")
+				|| qName.equalsIgnoreCase("relation")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:rights")
+				|| qName.equalsIgnoreCase("rights")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:alternative")
+				|| qName.equalsIgnoreCase("alternative")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:created")
+				|| qName.equalsIgnoreCase("created")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:extent")
+				|| qName.equalsIgnoreCase("extent")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:medium")
+				|| qName.equalsIgnoreCase("medium")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:isVersionOf")
+				|| qName.equalsIgnoreCase("isVersionOf")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:hasVersion")
+				|| qName.equalsIgnoreCase("hasVersion")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:isReplacedBy")
+				|| qName.equalsIgnoreCase("isReplacedBy")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:replaces")
+				|| qName.equalsIgnoreCase("replaces")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:isRequiredBy")
+				|| qName.equalsIgnoreCase("isRequiredBy")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:requires")
+				|| qName.equalsIgnoreCase("requires")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:isPartOf")
+				|| qName.equalsIgnoreCase("isPartOf")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:hasPart")
+				|| qName.equalsIgnoreCase("hasPart")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:isReferencedBy")
+				|| qName.equalsIgnoreCase("isReferencedBy")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:references")
+				|| qName.equalsIgnoreCase("references")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:isFormatOf")
+				|| qName.equalsIgnoreCase("isFormatOf")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:hasFormat")
+				|| qName.equalsIgnoreCase("hasFormat")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:conformsTo")
+				|| qName.equalsIgnoreCase("conformasTo")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:spatial")
+				|| qName.equalsIgnoreCase("spatial")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:temporal")
+				|| qName.equalsIgnoreCase("temporal")) {
+			
+		}
+		if (qName.equalsIgnoreCase("ne:geolocation")
+				|| qName.equalsIgnoreCase("geolocation")) {
+			
+		}
+
+	}
+
+	public void checkHeaderSection(String qName) {
 
 		if (qName.equalsIgnoreCase("ne:id") || qName.equalsIgnoreCase("id")) {
-			headerType.setId(true);
+			doc.add(new Field("header.id", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
 		if (qName.equalsIgnoreCase("ne:collectionId")
 				|| qName.equalsIgnoreCase("collectionId")) {
-
-			headerType.setCollectionId(true);
+			doc.add(new Field("header.collectionId", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
 
-		if (qName.equalsIgnoreCase("ne:createdOn") || qName.equalsIgnoreCase("createdOn")) {
-			headerType.setCreatedOn(true);
+		if (qName.equalsIgnoreCase("ne:createdOn")
+				|| qName.equalsIgnoreCase("createdOn")) {
+
+			doc.add(new Field("header.createdOn", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:createdBy")|| qName.equalsIgnoreCase("createdBy")) {
-
-			headerType.setCreatedBy(true);
+		if (qName.equalsIgnoreCase("ne:createdBy")
+				|| qName.equalsIgnoreCase("createdBy")) {
+			doc.add(new Field("header.createdBy", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:lastModifiedOn")||qName.equalsIgnoreCase("lastModifiedOn")) {
-
-			headerType.setLastModifiedOn(true);
+		if (qName.equalsIgnoreCase("ne:lastModifiedOn")
+				|| qName.equalsIgnoreCase("lastModifiedOn")) {
+			doc.add(new Field("header.lastModifiedOn", tmpValue,
+					Field.Store.YES, Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:lastModifiedBy")||qName.equalsIgnoreCase("lastModifiedBy")) {
-
-			headerType.setLastModifiedOn(true);
+		if (qName.equalsIgnoreCase("ne:lastModifiedBy")
+				|| qName.equalsIgnoreCase("lastModifiedBy")) {
+			doc.add(new Field("header.lastModifiedBy", tmpValue,
+					Field.Store.YES, Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:deletedOn")||qName.equalsIgnoreCase("deletedOn")) {
-
-			headerType.setDeletedOn(true);
+		if (qName.equalsIgnoreCase("ne:deletedOn")
+				|| qName.equalsIgnoreCase("deletedOn")) {
+			doc.add(new Field("header.deletedOn", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
 
-		if (qName.equalsIgnoreCase("ne:deletedBy")||qName.equalsIgnoreCase("deletedBy")) {
-
-			headerType.setDeletedBy(true);
+		if (qName.equalsIgnoreCase("ne:deletedBy")
+				|| qName.equalsIgnoreCase("deletedBy")) {
+			doc.add(new Field("header.deletedBy", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:version")||qName.equalsIgnoreCase("version")) {
-
-			headerType.setVersion(true);
+		if (qName.equalsIgnoreCase("ne:version")
+				|| qName.equalsIgnoreCase("version")) {
+			doc.add(new Field("header.version", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:status")||qName.equalsIgnoreCase("status")) {
+		if (qName.equalsIgnoreCase("ne:status")
+				|| qName.equalsIgnoreCase("status")) {
 
-			headerType.setStatus(true);
+			doc.add(new Field("header.status", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:access")||qName.equalsIgnoreCase("access")) {
-
-			headerType.setAccess(true);
+		if (qName.equalsIgnoreCase("ne:access")
+				|| qName.equalsIgnoreCase("access")) {
+			doc.add(new Field("header.access", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:validated")||qName.equalsIgnoreCase("validated")) {
-
-			headerType.setValidated(true);
+		if (qName.equalsIgnoreCase("ne:validated")
+				|| qName.equalsIgnoreCase("validated")) {
+			doc.add(new Field("header.validated", tmpValue, Field.Store.YES,
+					Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:metadataLanguages")||qName.equalsIgnoreCase("metadataLanguages")) {
-			headerType.setMetadataLangs(true);
 
+		if (qName.equalsIgnoreCase("ne:metadataLanguage")
+				|| qName.equalsIgnoreCase("metadataLanguage")) {
+			doc.add(new Field("header.metadataLanguages.language", tmpValue,
+					Field.Store.YES, Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:metadataLanguage")||qName.equalsIgnoreCase("metadataLanguage")) {
-
-			headerType.setMetadataLang(true);
+		if (qName.equalsIgnoreCase("ne:presentedToII")
+				|| qName.equalsIgnoreCase("presentedToII")) {
+			doc.add(new Field("header.presentedToII", tmpValue,
+					Field.Store.YES, Field.Index.NOT_ANALYZED));
 		}
-		if (qName.equalsIgnoreCase("ne:presentedToII")||qName.equalsIgnoreCase("presentedToII")) {
-
-			headerType.setPresentedToII(true);
-		}
-		if (qName.equalsIgnoreCase("ne:disseminatedToBiocase")||qName.equalsIgnoreCase("disseminatedToBiocase")) {
-
-			headerType.setDisseminatedToBiocase(true);
+		if (qName.equalsIgnoreCase("ne:disseminatedToBiocase")
+				|| qName.equalsIgnoreCase("disseminatedToBiocase")) {
+			doc.add(new Field("header.disseminatedToBiocase", tmpValue,
+					Field.Store.YES, Field.Index.NOT_ANALYZED));
 		}
 
 	}
 
 	public void characters(char ch[], int start, int length) {
 
-		if (headerType.isId()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.id", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setId(false);
-		}
-		if (headerType.isCollectionId()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.collectionId", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setCollectionId(false);
-		}
-		if (headerType.isCreatedOn()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.createdOn", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setCreatedOn(false);
-		}
-		if (headerType.isCreatedBy()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.createdBy", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setCreatedBy(false);
-		}
-		if (headerType.isLastModifiedOn()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.lastModifiedOn", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setLastModifiedOn(false);
-		}
-		if (headerType.isLastModifiedBy()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.lastModifiedBy", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setLastModifiedBy(false);
-		}
-		if (headerType.isDeletedOn()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.deletedOn", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setDeletedOn(false);
-		}
-		if (headerType.isDeletedBy()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.deletedBy", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setDeletedBy(false);
-		}
-		if (headerType.isVersion()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.version", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setVersion(false);
-		}
-		if (headerType.isStatus()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.status", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setStatus(false);
-		}
-		if (headerType.isAccess()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.access", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setAccess(false);
-		}
-		if (headerType.isValidated()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.validated", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setValidated(false);
-		}
-
-		if (headerType.isMetadataLangs()) {
-
-			headerType.setMetadataLangs(false);
-		}
-		if (headerType.isMetadataLang()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.metadataLanguages.metadataLanguage", id,
-					Field.Store.YES, Field.Index.NOT_ANALYZED));
-			headerType.setMetadataLang(false);
-		}
-		if (headerType.isPresentedToII()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.presentedToII", id, Field.Store.YES,
-					Field.Index.NOT_ANALYZED));
-			headerType.setPresentedToII(false);
-		}
-		if (headerType.isDisseminatedToBiocase()) {
-			String id = new String(ch, start, length);
-
-			doc.add(new Field("header.disseminatedToBiocase", id,
-					Field.Store.YES, Field.Index.NOT_ANALYZED));
-			headerType.setPresentedToII(false);
-
-		}
+		tmpValue = new String(ch, start, length);
 
 	}
 
