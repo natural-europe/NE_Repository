d3d0bff6316ca25b933b12a816535956a202854c
vogias <vogias@admin.grnet.gr>
2013-06-03 13:33:19 +0300

    remove v6

--- NE_Repository_Exp_v6/src/net/sourceforge/minor/lucene/core/searcher/IndexSearchDelegate.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.minor.lucene.core.searcher;
-
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.TopDocs;
-
-public interface IndexSearchDelegate {
-	
-	String result(TopDocs topDocs, IndexSearcher searcher) throws Exception;
-}
